8e737564950d8f0e88a9cada01f67228723ef056
Roman Glushko <roman.glushko.m@gmail.com>
2024-04-07 21:02:56 +0300

    Migrate the introduction (partially), fixed logo, images, fixed left menu order and titles

---
+++ components/Logo.tsx
@@ -0,0 +1,14 @@
+import LogoImg from '../images/glide_bird_in_circle.png';
+
+export function Logo() {
+    return (
+        <div style={{display: 'flex', alignItems: 'center'}}>
+            <img
+                src={LogoImg.src}
+                alt="Glide Logo"
+                style={{width: '40px', marginRight: '5px'}}
+            />
+            <span>Glide</span>
+        </div>
+    );
+}
