028fec54f7ec4d64841913d776d48866625cdd14
Roman Glushko <roman.glushko.m@gmail.com>
2024-04-23 18:33:07 +0300

    ✨ Style callouts of different types (#25)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class', 'html[class~="dark"]'],
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  variants: {
+    extend: {
+    },
+  },
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
